26e0679afbc92b1ffdcbdbadf76236ebdca1db13
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2020-11-03 16:39:33 -0500

    Added animations
    Idle animation, front row attack animation and back row attack animation have been added to new scene.

---
+++ Assets/Scripts/AnimationPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationPlayer : MonoBehaviour
+{
+    Animator anim;
+    public GameObject image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = image.GetComponent<Animator>();
+        anim.runtimeAnimatorController = Resources.Load("Animations/Wyvern/Idle/Idle") as RuntimeAnimatorController;
+    }
+
+    public void FrontRowAnimation()
+    {
+        anim = image.GetComponent<Animator>();
+        anim.runtimeAnimatorController = Resources.Load("Animations/Wyvern/Front/Front") as RuntimeAnimatorController;
+    }
+
+    public void BackRowAnimation()
+    {
+        anim = image.GetComponent<Animator>();
+        anim.runtimeAnimatorController = Resources.Load("Animations/Wyvern/Back/Back") as RuntimeAnimatorController;
+    }
+}
