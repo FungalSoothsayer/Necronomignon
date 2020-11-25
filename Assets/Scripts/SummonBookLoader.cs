6c872562bd32c32ff0987f5f6ab2da355601d2f0
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2020-11-25 15:40:55 -0500

    Added sorting by name, type and tier
    Need to set default to sort by tier.
    Need to add sorting to create squad page.

--- Assets/Scripts/SummonBookLoader.cs
+++ Assets/Scripts/SummonBookLoader.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using System;
 
 public class SummonBookLoader : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class SummonBookLoader : MonoBehaviour
     public List<GameObject> beastTexts;
     public List<Image> slots;
 
+    List<Beast> sorted = new List<Beast>();
     public List<string> summonedNames = new List<string>();
     public List<string> summonedImages = new List<string>();
     public List<Beast> summoned = new List<Beast>();
@@ -35,6 +37,7 @@ public class SummonBookLoader : MonoBehaviour
             summonedNames.Add(bl.Beasts[x].name);
             summonedImages.Add(bl.Beasts[x].static_img);
             summoned.Add(bl.Beasts[x]);
+            sorted.Add(bl.Beasts[x]);
         }
 
         SetImages();
@@ -45,19 +48,27 @@ public class SummonBookLoader : MonoBehaviour
     {
         for (int x = 0 + (counter * 6); x < slots.Count + (counter * 6); x++)
         {
-            if (summoned.Count >= x + 1)
+            if (sorted.Count >= x + 1)
             {
                 slots[x % 6].gameObject.SetActive(true);
                 slots[x % 6].sprite = Resources.Load<Sprite>(summonedImages[x]);
                 beastTexts[x % 6].GetComponent<Text>().text = summonedNames[x];
 
                 //Make not summoned beasts transparent
-                if (summoned[x].tier == 0)
+                if (sorted[x].tier == 0)
                 {
                     var tempColor = slots[x % 6].color;
                     tempColor.a = .5f;
                     slots[x % 6].color = tempColor;
                 }
+                
+                else
+                {
+                    var tempColor = slots[x % 6].color;
+                    tempColor.a = 1f;
+                    slots[x % 6].color = tempColor;
+                }
+                
             }
             else
             {
@@ -74,11 +85,11 @@ public class SummonBookLoader : MonoBehaviour
         {
             back.SetActive(true);
         }
-        if ((counter * 6) + 6 >= summoned.Count)
+        if ((counter * 6) + 6 >= sorted.Count)
         {
             forward.SetActive(false);
         }
-        else if ((counter * 6) + 6 < summoned.Count)
+        else if ((counter * 6) + 6 < sorted.Count)
         {
             forward.SetActive(true);
         }
@@ -115,8 +126,8 @@ public class SummonBookLoader : MonoBehaviour
         {
             if (EventSystem.current.currentSelectedGameObject.name == "Slot" + x)
             {
-                beastName = summoned[(counter * 6) + x - 1].name;
-                if (summoned[(counter * 6) + x - 1].tier == 0)
+                beastName = summonedNames[(counter * 6) + x - 1];
+                if (sorted[(counter * 6) + x - 1].tier == 0)
                 {
                     SceneManager.LoadScene(beastName + "Page1");
                 }
@@ -126,5 +137,94 @@ public class SummonBookLoader : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void SortImagesDropdown(int value)
+    {
+        if(value == 0)
+        {
+            SortByTier();
+        }
+        else if (value == 1)
+        {
+            SortByType();
+        }
+        else if (value == 2)
+        {
+            SortByName();
+        }
+    }
+
+    void SortByTier()
+    {
+        sorted.Clear();
+        summonedImages.Clear();
+        summonedNames.Clear();
+
+        for (int x = 5; x >= 0; x--)
+        {
+            for (int y = 0; y < summoned.Count; y++)
+            {
+                if (summoned[y].tier == x)
+                {
+                    sorted.Add(summoned[y]);
+                    summonedImages.Add(summoned[y].static_img);
+                    summonedNames.Add(summoned[y].name);
+                }
+            }
+        }
+        
+        SetImages();
+    }
+
+    //Not sure how to do this better...
+    void SortByType()
+    {
+        sorted.Clear();
+        summonedImages.Clear();
+        summonedNames.Clear();
+
+        for (int x = 0; x <= Enum.GetNames(typeof(Beast.types)).Length; x++)
+        {
+            for (int y = 0; y < summoned.Count; y++)
+            {
+                if ((int)summoned[y].type == x) 
+                {
+                    sorted.Add(summoned[y]);
+                    summonedImages.Add(summoned[y].static_img);
+                    summonedNames.Add(summoned[y].name);
+                }
+            }
+        }
+
+        SetImages();
+    }
+
+    void SortByName()
+    {
+        sorted.Clear();
+
+        List<string> names = new List<string>();
+
+        foreach(Beast b in summoned)
+        {
+            names.Add(b.name);
+        }
+
+        for(int x = 0; x < names.Count; x++)
+        {
+            for(int y = 0; y < summoned.Count; y++)
+            {
+                if(names[x] == summoned[y].name)
+                {
+                    sorted.Add(summoned[y]);
+                }
+            }
+        }
+
+        summonedImages.Sort();
+        summonedNames.Sort();
+        
+        SetImages();
     }
 }
