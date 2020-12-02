d5912187bdfd1d836a155eb38c4d9e8eec98cf33
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2020-12-02 13:29:46 -0500

    fixed demotarget

--- Assets/Scripts/Organize and Create Squads/DemoTarget.cs
+++ Assets/Scripts/Organize and Create Squads/DemoTarget.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/**
+ * Handles the mouse movement for attacks in the demo battle scene
+ */
 public class DemoTarget : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public DemoBattleManager demoBattleManager;
 
     private bool mouse_over = false;
 
+    // Update is called once per frame
     void Update()
     {
+        // Attacks every time the target is clicked
         if (mouse_over)
         {
             if (Input.GetMouseButtonDown(0))
@@ -20,13 +25,13 @@ public class DemoTarget : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         }
     }
 
-    //When the cursor is over this image, make mouse_over true
+    // When the cursor is over this image, make mouse_over true
     public void OnPointerEnter(PointerEventData eventData)
     {
         mouse_over = true;
     }
 
-    //When cursor leaves this image, make mouse_over false
+    // When cursor leaves this image, make mouse_over false
     public void OnPointerExit(PointerEventData eventData)
     {
         mouse_over = false;
