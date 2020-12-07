9c7bcb4382edfa4342ec4ef4f91b0b202c541c9e
FungalSoothsayer <70238977+FungalSoothsayer@users.noreply.github.com>
2020-12-07 14:51:26 -0500

    Added Comments to Sound.cs Script

--- Assets/Scripts/Data/Sound.cs
+++ Assets/Scripts/Data/Sound.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine.Audio;
 using UnityEngine;
-
+// Since we removed MonoBehavior, this is important so Unity Recognize the script
 [System.Serializable]
 public class Sound 
 {
-    
+    //This class  hold the attributes of a Sound object 
+
     public string name;
 
+    //This is the Mp3 clip 
     public AudioClip clip;
 
+    // These are to control volume and pitch of the Clip
     [Range(0f, 1f)]
     public float volume;
     [Range(.1f, 3f)]
