a3767986c65b74aaebcf6247b84e103db4974052
FungalSoothsayer <70238977+FungalSoothsayer@users.noreply.github.com>
2020-12-08 10:15:38 -0500

    Added Comments To QuizChoices.cs

--- Assets/Scripts/Summon/QuizChoice.cs
+++ Assets/Scripts/Summon/QuizChoice.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*
+ * This Class Should handle everything Quizz Related, After reading the short stories for individual beasts, a quiz should pop up, and reading comprehension questions 
+ * will be asked to the player
+ *  WORK IN PROGRESS!!!
+ */
 public class QuizChoice : MonoBehaviour
 {
 
