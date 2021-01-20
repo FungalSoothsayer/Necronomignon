212ef3254a1667fac12d439fd7f3aa9a4881a8cf
SidGads <sidneygad@hotmail.com>
2021-01-20 15:43:36 -0500

    fix and update

--- Assets/Scripts/Data/Saving.cs
+++ Assets/Scripts/Data/Saving.cs
@@ -21,10 +21,28 @@ public class Saving : MonoBehaviour
     {
         EasySave.Save<BeastList>("playerBL",BeastManager.beastsList);
         EasySave.Save<int>("level", LevelChecker.levels);
+        if (SquadData.squad1Saved)
+        {
+            EasySave.Save<List<Beast>>("squad1",SquadData.squad1);
+        }
+        if (SquadData.squad2Saved)
+        {
+            EasySave.Save<List<Beast>>("squad2", SquadData.squad2);
+        }
     }
     public void loadAll()
     {
         BeastManager.beastsList = EasySave.Load<BeastList>("playerBL");
         LevelChecker.levels = EasySave.Load<int>("level");
+        if (EasySave.Load<List<Beast>>("squad1") != null && EasySave.Load<List<Beast>>("squad1").Count>0)
+        {
+            SquadData.squad1 = EasySave.Load<List<Beast>>("squad1");
+            SquadData.squad1Saved = true;
+        }
+        if (EasySave.Load<List<Beast>>("squad2") != null && EasySave.Load<List<Beast>>("squad2").Count > 0)
+        {
+            SquadData.squad2 = EasySave.Load<List<Beast>>("squad2");
+            SquadData.squad2Saved = true;
+        }
     }
 }
