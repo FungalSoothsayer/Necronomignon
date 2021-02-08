af4eb69e515c4d17e9e6b96151676fe317d43898
MiguelACG <63476768+MiguelACG@users.noreply.github.com>
2021-02-08 18:07:10 -0500

    Begin Dialogue Scene
    Beginning to set up dialogue scene.

---
+++ Assets/Scripts/Dialog/DialogueManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+using UnityEngine.SceneManagement;
+
+public class DialogManager : MonoBehaviour
+{
+
+    public NPCConversation conversation;
+    public string current;
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = SceneManager.GetActiveScene().name;
+        conversation = gameObject.GetComponent(typeof(NPCConversation)) as NPCConversation;
+
+        if (current == "DialogScene")
+            ConversationManager.Instance.StartConversation(conversation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
