b367af87293323352ed7dba6762dcda97ea5f25f
MiguelACG <63476768+MiguelACG@users.noreply.github.com>
2021-02-23 15:10:14 -0500

    Summoned screen edited

--- Assets/Scripts/Summon/AnimationPlayer.cs
+++ Assets/Scripts/Summon/AnimationPlayer.cs
@@ -15,8 +15,24 @@ public class AnimationPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*
         image.GetComponent<Animator>().runtimeAnimatorController = Resources.Load
             ("Animations/" + SummonBookLoader.beastName + "/" + SummonBookLoader.beastName + "_Controller") as RuntimeAnimatorController;
+        */
+        image.GetComponent<Image>().sprite = Resources.Load<Sprite>("Static_Images/EmptyRectangle");
+
+
+        //Prefab setting
+        GameObject beastPrefab = (GameObject)Instantiate(Resources.Load($"Prefabs/Beasts/{SummonBookLoader.beastName}"));
+        beastPrefab.transform.SetParent(GameObject.Find($"Image").transform);
+        beastPrefab.transform.localPosition = new Vector3(0, 0);
+        beastPrefab.transform.localRotation = Quaternion.identity;
+        beastPrefab.transform.localScale = new Vector3(10, 10);
+        Animator animator = beastPrefab.GetComponent<Animator>();
+        animator.enabled = false;
+
+
+
     }
 
     //Front Row Attack
