233493c8e89ed3e6e2b4720cde0ebff89fd5f066
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2021-03-01 14:29:35 -0500

    small change

--- Assets/Scripts/Battle/Beasts/SovereignDragon_Script.cs
+++ Assets/Scripts/Battle/Beasts/SovereignDragon_Script.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class SovereignDragon_Script : MonoBehaviour, Parent_Beast
 {
     BattleManager battleManager;
+    [SerializeField] GameObject frontPrefab;
 
     void Start()
     {
